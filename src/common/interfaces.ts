31f0427dd6fbb939c5c1c809de7c45e3cca14eef
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-04-29 10:49:16 +1000

    Added Repository and Owner to interfaces

---
+++ src/common/interfaces.ts
@@ -0,0 +1,13 @@
+export type Owner = {
+  id: string
+  avatar_url: string
+}
+
+export type Repository = {
+  id: string
+  name: string
+  full_name: string
+  owner: Owner
+  html_url: string
+  description: string
+}
